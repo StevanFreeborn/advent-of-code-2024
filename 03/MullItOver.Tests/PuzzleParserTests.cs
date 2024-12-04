f54043c9dfb5b7444a3cf47bda1ec733dc00c7ec
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-12-03 22:25:18 -0600

    feat: initial project setup for day 3

---
+++ 03/MullItOver.Tests/PuzzleParserTests.cs
@@ -0,0 +1,14 @@
+﻿namespace MullItOver.Tests;
+
+public class PuzzleParserTests
+{
+  private readonly PuzzleParser _parser = new();
+  
+  [Test]
+  public async Task Parse_WhenInputIsEmpty_ReturnsEmptyList()
+  {
+    var result = _parser.Parse("");
+    
+    await Assert.That(result).IsEquivalentTo(new List<Instruction>());
+  }
+}
---
+++ 03/MullItOver/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+if (args.Length is 0)
+{
+  Console.WriteLine("Please provide a path to the input file.");
+  return;
+}
+
+if (File.Exists(args[0]) is false)
+{
+  Console.WriteLine("The provided file does not exist.");
+  return;
+}
+
+var isPart2 = args.Length is 2 && args[1] is "part2";
+var input = await File.ReadAllTextAsync(args[0]);
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+var instructions = new PuzzleParser().Parse(input);
+
+var results = 0;
+
+stopwatch.Stop();
+Console.WriteLine($"The sum of instructions is {results}. ({stopwatch.ElapsedMilliseconds}ms)");
+
+class PuzzleParser
+{
+  public List<Instruction> Parse(string input)
+  {
+    return [];
+  }
+}
+
+class Instruction(int multiplicandOne, int multiplicandTwo)
+{
+}
