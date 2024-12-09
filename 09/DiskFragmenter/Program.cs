3eb635d42f8bce30d40bdcd67878f8646491aa46
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-12-08 23:18:17 -0600

    chore: setup for day 9

---
+++ 09/DiskFragmenter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+if (args.Length is 0)
+{
+  Console.WriteLine("Please provide a path to the input file.");
+  return;
+}
+
+if (File.Exists(args[0]) is false)
+{
+  Console.WriteLine("The provided file does not exist.");
+  return;
+}
+
+var isPart2 = args.Length is 2 && args[1] is "part2";
+var input = await File.ReadAllLinesAsync(args[0]);
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+// TODO: Implement solution
+
+stopwatch.Stop();
+Console.WriteLine($". ({stopwatch.ElapsedMilliseconds}ms)");
