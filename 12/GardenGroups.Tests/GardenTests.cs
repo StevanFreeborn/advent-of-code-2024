0577f2af7bcad13bba69c3f8859652b868a68c3d
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-12-13 09:03:40 -0600

    feat: stalled on day 12 part 2

---
+++ 12/GardenGroups.Tests/GardenTests.cs
@@ -0,0 +1,100 @@
+﻿namespace GardenGroups.Tests;
+
+public class GardenTests
+{
+  [Test]
+  [MethodDataSource(nameof(CalculateFencePriceBasedOnPerimeterTestCases))]
+  public async Task CalculateFencePriceBasedOnPerimeter_WhenCalled_ItShouldReturnExpectedValue(CalculateFencePriceTestCase testCase)
+  {
+    var result = Garden.From(testCase.Input).CalculateFencePriceBasedOnPerimeter();
+
+    await Assert.That(result).IsEqualTo(testCase.ExpectedPrice);
+  }
+  
+  [Test]
+  [MethodDataSource(nameof(CalculateFencePriceBasedOnSidesTestCases))]
+  public async Task CalculateFencePriceBasedOnSides_WhenCalled_ItShouldReturnExpectedValue(CalculateFencePriceTestCase testCase)
+  {
+    var result = Garden.From(testCase.Input).CalculateFencePriceBasedOnSides();
+
+    await Assert.That(result).IsEqualTo(testCase.ExpectedPrice);
+  }
+
+  public static IEnumerable<Func<CalculateFencePriceTestCase>> CalculateFencePriceBasedOnSidesTestCases()
+  {
+    yield return () => new(["AAAA", "BBCD", "BBCC", "EEEC"], 80);
+    
+    yield return () => new(
+      [
+        "EEEEE",
+        "EXXXX",
+        "EEEEE",
+        "EXXXX",
+        "EEEEE",
+      ],
+      236
+    );
+    
+    yield return () => new(
+      [
+        "AAAAAA",
+        "AAABBA",
+        "AAABBA",
+        "ABBAAA",
+        "ABBAAA",
+        "AAAAAA",
+      ],
+      368
+    );
+    
+    yield return () => new(
+      [
+        "RRRRIICCFF",
+        "RRRRIICCCF",
+        "VVRRRCCFFF",
+        "VVRCCCJFFF",
+        "VVVVCJJCFE",
+        "VVIVCCJJEE",
+        "VVIIICJJEE",
+        "MIIIIIJJEE",
+        "MIIISIJEEE",
+        "MMMISSJEEE",
+      ],
+      1206
+    );
+  }
+
+  public static IEnumerable<Func<CalculateFencePriceTestCase>> CalculateFencePriceBasedOnPerimeterTestCases()
+  {
+    yield return () => new(["AAAA", "BBCD", "BBCC", "EEEC"], 140);
+    
+    yield return () => new(
+      [
+        "OOOOO",
+        "OXOXO",
+        "OOOOO",
+        "OXOXO",
+        "OOOOO",
+      ], 
+      772
+    );
+    
+    yield return () => new(
+      [
+       "RRRRIICCFF",
+       "RRRRIICCCF",
+       "VVRRRCCFFF",
+       "VVRCCCJFFF",
+       "VVVVCJJCFE",
+       "VVIVCCJJEE",
+       "VVIIICJJEE",
+       "MIIIIIJJEE",
+       "MIIISIJEEE",
+       "MMMISSJEEE",
+      ],
+      1930
+    );
+  }
+  
+  public record CalculateFencePriceTestCase(string[] Input, int ExpectedPrice);
+}
---
+++ 12/GardenGroups.Tests/RegionTests.cs
@@ -0,0 +1,34 @@
+﻿namespace GardenGroups.Tests;
+
+public class RegionTests
+{
+  [Test]
+  public async Task Region_WhenInstanceConstructed_ItShouldSetPlantTypeProperty()
+  {
+    var plantType = 'A';
+
+    var region = Region.From('A');
+
+    await Assert.That(region.PlantType).IsEqualTo(plantType);
+  }
+
+  [Test]
+  public async Task IncreasePerimeter_WhenCalled_ItShouldIncreaseRegionPerimeter()
+  {
+    var region = Region.From('A');
+    
+    region.IncreasePerimeter();
+
+    await Assert.That(region.Perimeter).IsEqualTo(1);
+  }
+  
+  [Test]
+  public async Task CalculatePrice_WhenCalled_ItShouldReturnExpectedValue()
+  {
+    var region = Region.From('A');
+
+    var result = region.CalculateFencePriceBasedOnPerimeter();
+
+    await Assert.That(result).IsEqualTo(0);
+  }
+}
