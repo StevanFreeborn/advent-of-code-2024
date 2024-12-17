930dbb94fd593851865d10c450a163f01b0ae54a
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-12-17 17:23:12 -0600

    chore: remove comments

--- 16/ReindeerMaze/Program.cs
+++ 16/ReindeerMaze/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System.Diagnostics;
 
-// if (args.Length is 0)
-// {
-//   Console.WriteLine("Please provide a path to the input file.");
-//   return;
-// }
-//
-// if (File.Exists(args[0]) is false)
-// {
-//   Console.WriteLine("The provided file does not exist.");
-//   return;
-// }
+if (args.Length is 0)
+{
+  Console.WriteLine("Please provide a path to the input file.");
+  return;
+}
+
+if (File.Exists(args[0]) is false)
+{
+  Console.WriteLine("The provided file does not exist.");
+  return;
+}
 
 var isPart2 = args.Length is 2 && args[1] is "part2";
-var input = await File.ReadAllLinesAsync(Path.Combine(AppContext.BaseDirectory, "INPUT.txt"));
+var input = await File.ReadAllLinesAsync(args[0]);
 
 var stopwatch = new Stopwatch();
 stopwatch.Start();
